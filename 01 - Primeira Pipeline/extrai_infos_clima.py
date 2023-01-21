dc95c818ac13e84173fe18617e98ce1f0c5dd858
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-01-21 12:14:33 -0300

    style: documentação do que cada parte do código faz

--- 01 - Primeira Pipeline/extrai_infos_clima.py
+++ 01 - Primeira Pipeline/extrai_infos_clima.py
@@ -5,13 +5,14 @@ from os.path import join
 from datetime import datetime, timedelta
 
 
+# Parametros de data
 data_inicio = datetime.today()
 data_fim = data_inicio + timedelta(days=7)
 
 data_inicio = data_inicio.strftime('%Y-%m-%d')      # type: ignore
 data_fim = data_fim.strftime('%Y-%m-%d')            # type: ignore
 
-
+# Key e cidade para busca na API
 city = 'Boston'
 key = 'S2288F34GLCJJ42JH4WAYS4DS'
 
@@ -22,7 +23,9 @@ URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/' \
 
 dados = pd.read_csv(URL)
 
+# Criação da pasta da salvar os dados buscados pela API
 dir_data_path = os.path.abspath(
     f'01 - Primeira Pipeline/dados/semana_{data_inicio}')
 dir_path = Path(dir_data_path)
 dir_path.mkdir(parents=True, exist_ok=True)
+
