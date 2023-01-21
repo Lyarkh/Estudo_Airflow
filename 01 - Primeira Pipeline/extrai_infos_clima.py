955d6f40fb309273a606c763f5cc8056719e531b
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-01-21 11:54:49 -0300

    feat: variaveis importantes para a busca dos dados

---
+++ 01 - Primeira Pipeline/extrai_infos_clima.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+from os.path import join
+from datetime import datetime, timedelta
+
+
+data_inicio = datetime.today()
+data_fim = data_inicio + timedelta(days=7)
+
+data_inicio = data_inicio.strftime('%Y-%m-%d')      # type: ignore
+data_fim = data_fim.strftime('%Y-%m-%d')            # type: ignore
+
+
+city = 'Boston'
+key = 'S2288F34GLCJJ42JH4WAYS4DS'
