f48b46e806e36015d174ae9a71cda47774d63f78
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-01-21 12:00:15 -0300

    feat: url de busca na API'

--- 01 - Primeira Pipeline/extrai_infos_clima.py
+++ 01 - Primeira Pipeline/extrai_infos_clima.py
@@ -13,3 +13,8 @@ data_fim = data_fim.strftime('%Y-%m-%d')            # type: ignore
 
 city = 'Boston'
 key = 'S2288F34GLCJJ42JH4WAYS4DS'
+
+
+URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/' \
+    f'services/timeline/{city}/{data_inicio}/{data_fim}?' \
+    f'unitGroup=metric&include=days?key={key}&contentType=csv'
