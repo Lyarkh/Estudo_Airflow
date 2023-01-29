78767ff7d8474c8b3e49d5e8ccb709672ef34874
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-01-29 14:53:48 -0300

    refactor: modificando path do arquivo presente com o código de extração de dados

---
+++ 01_Primeira_Pipeline/Airflow_Dags/dags/extrai_infos_clima.py
@@ -0,0 +1,39 @@
+import os
+import pandas as pd
+from pathlib import Path
+from datetime import datetime, timedelta
+
+
+class ExtraiInfosClima:
+    def __init__(self):
+        # Parametros de data
+        self.data_inicio = datetime.today()
+        self.data_fim = self.data_inicio + timedelta(days=7)
+
+        self.data_inicio = self.data_inicio.\
+            strftime('%Y-%m-%d')                                # type: ignore
+        self.data_fim = self.data_fim.strftime('%Y-%m-%d')      # type: ignore
+
+    def busca_dados(self):
+        # Key e cidade para busca na API
+        city = 'Boston'
+        key = 'S2288F34GLCJJ42JH4WAYS4DS'
+
+        URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/' \
+            f'services/timeline/{city}/{self.data_inicio}/{self.data_fim}?' \
+            f'unitGroup=metric&include=days&key={key}&contentType=csv'
+
+        dados = pd.read_csv(URL)
+
+        # Criação da pasta da salvar os dados buscados pela API
+        dir_data_path = os.path.abspath(
+            f'01 - Primeira Pipeline/dados/semana_{self.data_inicio}')
+        dir_path = Path(dir_data_path)
+        dir_path.mkdir(parents=True, exist_ok=True)
+
+        # salvando dados
+        dados.to_csv(dir_path / 'dados_brutos.csv', index=False)
+        dados[['datetime', 'tempmin', 'temp', 'tempmax']].\
+            to_csv(dir_path / 'temperaturas.csv', index=False)
+        dados[['datetime', 'description', 'icon']].\
+            to_csv(dir_path / 'condicoes.csv', index=False)
